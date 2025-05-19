e554905d44b87f20b1f82f586860a97cfba42762
w-whited <williamwhited17@gmail.com>
2025-05-19 15:27:01 -0500

    final push

--- jobs.sql
+++ jobs.sql
@@ -1,74 +1,93 @@
 --1.
-SELECT COUNT(*) 
+
+SELECT COUNT(*)
 FROM data_analyst_jobs;
 
 --2.
+
 SELECT *
 FROM data_analyst_jobs
-limit 10;
--- ExxonMobil
+LIMIT 10;
+--ExxonMobil
 
 --3.
-SELECT COUNT(*) as TN_count
+
+SELECT COUNT(*) 
 FROM data_analyst_jobs
 WHERE location = 'TN';
 
-SELECT COUNT(*) as TN_KY_count
+SELECT COUNT(*) 
 FROM data_analyst_jobs
-WHERE location = 'TN' or location = 'KY';
+WHERE location = 'TN' OR location = 'KY';
 
 --4.
-SELECT COUNT(*)
+
+SELECT count(*)
 FROM data_analyst_jobs
 WHERE location = 'TN' AND star_rating > 4;
 
 --5.
-SELECT count(*)
+
+SELECT COUNT(*)
 FROM data_analyst_jobs
-WHERE review_count between 500 AND 1000;
+WHERE review_count BETWEEN 500 AND 1000;
 
 --6.
-SELECT location AS state, ROUND(avg(star_rating), 2) AS avg_rating
+
+SELECT location AS state, ROUND(AVG(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
-WHERE LOCATION IS NOT NULL AND star_rating IS NOT NULL
-GROUP BY state
-ORDER BY avg_rating DESC;
+WHERE location IS NOT NULL AND star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating desc
+LIMIT 1;
 
 --7.
+
 SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs;
 
 --8.
-SELECT COUNT(DISTINCT title)
+
+SELECT count(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location = 'CA';
 
 --9.
-SELECT company, ROUND(AVG(star_rating),2) AS avg_star_rating
+
+SELECT COUNT(DISTINCT company)
 FROM data_analyst_jobs
-WHERE review_count > 5000 AND company IS NOT NULL
-GROUP BY company;
+WHERE review_count > 5000 AND company IS NOT NULL;
 
 --10.
-SELECT company, AVG(star_rating) AS avg_star_rating
+
+SELECT company, ROUND(AVG(star_rating),2) avg_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000 AND company IS NOT NULL
 GROUP BY company
-ORDER BY avg_star_rating DESC;
+ORDER BY avg_rating DESC
+LIMIT 1;
 
 --11.
+
 SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
-where title ILIKE '%Analyst%';
+WHERE title ILIKE '%analyst%';
 
 --12.
+
 SELECT *
 FROM data_analyst_jobs
 WHERE title NOT ILIKE '%analyst%' AND title NOT ILIKE '%analytics%';
 
---BONUS
-SELECT domain, COUNT(*) AS job_count
+--BONUS.
+
+SELECT domain, COUNT(*) AS num_hard_to_fill
 FROM data_analyst_jobs
 WHERE skill ILIKE '%SQL%' AND days_since_posting > 21 AND domain IS NOT NULL
 GROUP BY domain
-ORDER BY job_count DESC
+ORDER BY num_hard_to_fill DESC
+LIMIT 4;
+
+
+
+
